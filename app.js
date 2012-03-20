8a79a3e4f95c0156eaf05fd7edc90e4199c94eff
Evan Despault <evan11@gmail.com>
2012-03-19 23:41:09 -0700

    add amcharts chart

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, "(", port, ")", app.settings.env);
