ba0d9485141e92b37bee84c8290d61bd9832aa19
Evan Despault <evan11@gmail.com>
2012-03-28 00:27:48 -0700

    correct db name

--- dataprovider-memory.js
+++ dataprovider-memory.js
@@ -5,7 +5,7 @@ var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
 
 DataProvider = function(host, port) {
-	this.db = new Db('test', new Server(host, port, {auto_reconnect: true}, {}));
+	this.db = new Db('app3373805', new Server(host, port, {auto_reconnect: true}, {}));
 	this.db.open(function(){});
 };
 
