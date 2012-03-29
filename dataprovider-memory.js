b03b58b22d3dd24832cea384f6067c14af9469ba
Evan Despault <evan11@gmail.com>
2012-03-28 17:09:09 -0700

    adding db constructor

--- dataprovider-memory.js
+++ dataprovider-memory.js
@@ -8,7 +8,7 @@ var Db = require('mongodb').Db
 // Construct data provider
 DataProvider = function(host, port, app) {
 	console.log("app: " + app + ", host: " + host + ", port: " + port);
-//	this.db = new Db(app, new Server(host, port, {auto_reconnect: true}, {}));
+	this.db = new Db(app, new Server(host, port, {auto_reconnect: true}, {}));
 	console.log("this.db: " + this.db);
 //	this.db.open(function(){});
 //	this.db.authenticate("heroku", "75912ba0d1319f1a04622f9837a6604b", {});
